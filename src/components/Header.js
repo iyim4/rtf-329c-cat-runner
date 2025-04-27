06bfb94338da07e2cac73b29d040e096ce3ec306
iyim4 <iyim@utexas.edu>
2025-04-27 14:32:18 -0500

    get logo to render locally (webpack issue)

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import './Header.css'; // Optional: if you want to style your component
+import './Header.css';
+import cat_logo from './cat_logo.svg'
 
 const Header = () => {
   return (
     <header className="header">
-      <img src={"./cat_logo.svg"} alt="Cat logo" className="header-image" />
+      <img src={cat_logo} alt="Cat logo" className="header-image" />
       <br />
       <h1 className="header-title">Cat Runner</h1>
       <p className="header-description">
         Infinite running cat game inspired by the Chrome Dino game.
         Avoid the Cucumbers!
       </p>
-      {/* <img src={"./images/cat_1.png"} alt="Cat logo" className="header-image" /> */}
     </header>
   );
 };
